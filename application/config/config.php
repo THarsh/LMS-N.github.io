7815382e766cad788f458b69fe59a41089dcbf00
Harshana Hewage <harshana.hewage@gmail.com>
2017-06-29 12:41:47 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/lms_new/';
+$config['base_url'] = 'http://harshanahewage.me/LMS-N.github.io/';
 
 /*
 |--------------------------------------------------------------------------
