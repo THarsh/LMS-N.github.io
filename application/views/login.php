19bd20c456e8c62f8c2bdde5e947444f029fefd3
Harshana Hewage <harshana.hewage@gmail.com>
2017-06-29 13:48:44 +0530

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,24 +1,108 @@
- <?php include(FCPATH . "assets/includes/nav.php"); ?>
+  <?php include(FCPATH . "assets/includes/nav.php"); ?>
 
- <div class="container-fluid">
-	 
-	<div class="login-card"><img src="http://via.placeholder.com/500x500" class="profile-img-card">
-        <p class="profile-name-card"> </p>
-        <form class="form-signin"><span class="reauth-email"> </span>
-            <input class="form-control" type="email" required="" placeholder="Email address" autofocus="" id="inputEmail">
-            <input class="form-control" type="password" required="" placeholder="Password" id="inputPassword">
-            <div class="checkbox">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox">Remember me</label>
+<div class="container">
+<div class="row" style="padding-top:10px;">
+    <div class="col-md-6">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Sign in</h5>
+
+            </div>
+            <div class="ibox-content">
+                <form class="form-horizontal">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="form-group"><label>Email</label> <input placeholder="Email" class="form-control" type="Email"></div>
+
+
+                        <div class="form-group"><label>Password</label> <input placeholder="Password" class="form-control" type="password"></div>
+                    </div>
+
+
+
+
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <div>
+                                <center>
+                                    <button class="btn btn-sm btn-primary   m-t-n-xs" type="submit"><strong>Log in</strong></button>
+                                </center>
+                            </div>
+                            <hr>
+                            <center>
+                                </label><a href="#" id="reg_link" onclick="modle_pop();"  data-toggle="modal" >Create new account</a></lable>
+                            </center>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h5>Create a new account</h5>
+
+                </div>
+                <div class="ibox-content">
+                    <form class="form-horizontal">
+
+                        <div class="form-group"><label class="col-lg-2 control-label">Email</label>
+
+                            <div class="col-lg-10"><input placeholder="Email" class="form-control"
+                                                          type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group"><label class="col-lg-2 control-label">First Name</label>
+
+                            <div class="col-lg-10"><input placeholder="First Name" class="form-control"
+                                                          type="text">
+                            </div>
+                        </div>
+
+                        <div class="form-group"><label class="col-lg-2 control-label">Last Name</label>
+
+                            <div class="col-lg-10"><input placeholder="Last Name" class="form-control"
+                                                          type="text">
+                            </div>
+                        </div>
+
+                        <div class="form-group"><label class="col-lg-2 control-label">Phone Number</label>
+
+                            <div class="col-lg-10"><input placeholder="Phone Number" class="form-control"
+                                                          type="text">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group"><label class="col-lg-2 control-label">Password</label>
+
+                            <div class="col-lg-10"><input placeholder="Password" class="form-control"
+                                                          type="password"></div>
+                        </div>
+
+                        <div class="form-group"><label class="col-lg-2 control-label">Confirm
+                                Password</label>
+
+                            <div class="col-lg-10"><input placeholder="Confirm  Password"
+                                                          class="form-control" type="password"></div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-lg-offset-2 col-lg-10">
+                                <div>
+                                    <button class="btn btn-sm btn-primary   m-t-n-xs" type="submit"><strong>Log
+                                            in</strong></button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
-        </form>
-        <a href="#" class="forgot-password">Forgot your password?</a>
-        <a class="text-warning" href="<?= base_url()?>index.php/welcome/registration" target="_blank"> Registration </a>
-	</div>
 
- </div>
-
+    </div>
+</div>
  
